22826d44733e2a080e300ed03deaf1d3c94210f3
aula7 <pc704@gmail.com>
2017-03-14 13:40:50 -0500

    Initial commit

---
+++ app/src/main/java/com/example/aula7/mystareas2/CreateAcountActivity.java
@@ -0,0 +1,17 @@
+package com.example.aula7.mystareas2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CreateAcountActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_acount);
+    }
+
+
+
+
+}
